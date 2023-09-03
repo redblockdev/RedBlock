6799fc06cc520de89a520864d0c04a0392a7541e
redblockdev <redblockdev@gmail.com>
2023-09-03 07:26:51 -0700

    Update Package name

--- build_msvc/bitcoin_config.h
+++ build_msvc/bitcoin_config.h
@@ -349,7 +349,7 @@
 #define PACKAGE_STRING "RedBlock 0.1.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "bitcoin"
+#define PACKAGE_TARNAME "redblock"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL "https://redblockcoin.com/"
